0ff5e5e2c273b2f14da2079f32f42af12773eb36
Adam Smelkowski <adamsmelkowski@gmail.com>
2025-11-22 18:54:20 +0100

    fix: Add redis instance to terraform

--- infra/compute.tf
+++ infra/compute.tf
@@ -26,6 +26,7 @@ resource "scaleway_container" "backend" {
   secret_environment_variables = {
     "DATABASE_URL" = "postgresql+asyncpg://gift_genie:${var.db_password}@${scaleway_rdb_instance.main.load_balancer[0].ip}:${scaleway_rdb_instance.main.load_balancer[0].port}/${scaleway_rdb_database.main.name}"
     "SECRET_KEY"   = var.db_password # Reusing for now
+    "REDIS_URL"    = "${scaleway_redis_cluster.main.public_network[0].ips[0]}:${scaleway_redis_cluster.main.public_network[0].port}"
   }
 }
 
